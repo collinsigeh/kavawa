b6200559a32b7e99b4e786a622d0bcbc99d84ab8
Collins Igeh <igehac@gmail.com>
2024-07-15 06:49:13 +0100

    implemented a display of entities created.

--- resources/views/livewire/entities/entity-list.blade.php
+++ resources/views/livewire/entities/entity-list.blade.php
@@ -1,3 +1,20 @@
 <div>
-    Here's a list of entities.
+    @if ($my_entities->count() < 1)
+        You have not created a business entity.
+    @else
+        <div class="row entity-list">
+            @foreach ($my_entities as $item)
+                <div class="col-sm-6 col-md-4 col-xl-3">
+                    <div class="entity-item">
+                        <a href="#">{{ $item->name }}</a>
+                        @if ($item->is_active)
+                            <i class="bi bi-check-circle float-end" style="color: #2e844a" title="Active"></i>
+                        @else
+                            <i class="bi bi-x-circle float-end" style="color: #e90000;" title="Inactive"></i>
+                        @endif
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    @endif
 </div>
